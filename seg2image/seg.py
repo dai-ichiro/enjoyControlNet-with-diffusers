52b5de0d7ea39f2140e3372596d56798bb84bda0
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-06-07 12:34:21 +0900

    Update seg.py

--- seg2image/seg.py
+++ seg2image/seg.py
@@ -3,16 +3,13 @@ from PIL import Image
 from huggingface_hub import hf_hub_download
 from diffusers.utils import load_image
 import torch
+from torch import nn
 import numpy
+import cv2
 
 image_processor = AutoImageProcessor.from_pretrained("openmmlab/upernet-convnext-xlarge")
 model = UperNetForSemanticSegmentation.from_pretrained("openmmlab/upernet-convnext-xlarge")
 
-'''
-filepath = hf_hub_download(
-    repo_id="hf-internal-testing/fixtures_ade20k", filename="ADE_val_00000001.jpg", repo_type="dataset"
-)
-'''
 image = load_image('1.jpg')
 
 inputs = image_processor(image, return_tensors="pt")
@@ -21,7 +18,6 @@ with torch.no_grad():
     outputs = model(**inputs)
     logits = outputs.logits
 
-from torch import nn
 logits = nn.functional.interpolate(outputs.logits.detach().cpu(),
                                     size=image.size[::-1], # (height, width)
                                     mode='bilinear',
@@ -29,17 +25,6 @@ logits = nn.functional.interpolate(outputs.logits.detach().cpu(),
 
 predicted = (logits.argmax(1))[0].numpy()
 
-import numpy as np 
-
 seg = np.where(predicted==12, 255, 0)
 
-import cv2
 cv2.imwrite('result.png', seg)
-
-
-
-'''
-logits = outputs.logits  # shape (batch_size, num_labels, height, width)
-list(logits.shape)
-[1, 150, 512, 512]
-'''
