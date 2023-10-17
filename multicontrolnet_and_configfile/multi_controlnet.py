5bae4d4ef66eac6631f4997a17c660648d10fcb1
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-10-17 22:33:18 +0900

    Add files via upload

---
+++ multicontrolnet_and_configfile/multi_controlnet.py
@@ -0,0 +1,88 @@
+import os
+import torch
+from diffusers import AutoPipelineForText2Image, ControlNetModel, EulerAncestralDiscreteScheduler
+from diffusers.utils import load_image
+
+import json
+
+config = json.load(open("config.json"))
+
+width = config["width"]
+height = config["height"]
+
+start = config["seed_first"]
+step = config["seed_step"]
+end = start + step * config["n_samples"]
+
+seeds = range(start, end, step)
+
+controlnet_list = []
+image_list = []
+controlnet_name_list = []
+
+for controlnet in config["controlnet"]:
+    if controlnet["enable"]:
+        controlnet_list.append(
+            ControlNetModel.from_pretrained(controlnet["path"], torch_dtype=torch.float16).to('cuda')
+        )
+        image_list.append(
+            load_image(controlnet["image"]).resize((width, height))
+        )
+        controlnet_name_list.append(
+            controlnet["name"]
+        )
+
+pipe = AutoPipelineForText2Image.from_pretrained(
+    config["model"],
+    controlnet=controlnet_list,
+    safety_checker=None,
+    torch_dtype=torch.float16).to('cuda')
+
+scheduler = config["scheduler"]
+match scheduler:
+    case 'pmdn':
+        from diffusers import  PNDMScheduler
+        pipe.scheduler = PNDMScheduler.from_config(pipe.scheduler.config)
+    case 'multistepdpm':
+        from diffusers import DPMSolverMultistepScheduler
+        pipe.scheduler = DPMSolverMultistepScheduler.from_config(
+            pipe.scheduler.config,
+            algorithm_type="sde-dpmsolver++",
+            use_karras_sigmas=True
+        )
+    case 'eulera':
+        from diffusers import EulerAncestralDiscreteScheduler
+        pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+    case _:
+        None
+
+for textual_inversion in config["textual_inversion"]:
+    if textual_inversion["enable"]:
+        file_path = textual_inversion["path"]
+        pipe.load_textual_inversion(file_path, token=textual_inversion["token"])
+        print(f"load {file_path}")
+
+prompt = config["prompt"]
+n_prompt = config["n_prompt"]
+
+print(f'prompt: {prompt}')
+print(f'negative prompt: {n_prompt}')
+
+for lora in config["lora"]:
+    if lora["enable"]:
+        file_path = lora["path"]
+        pipe.load_lora_weights(file_path)
+        print(f"load {file_path}")
+
+save_folder = "_".join(controlnet_name_list)
+os.makedirs(save_folder, exist_ok=True)
+
+for i in seeds:
+    image = pipe(
+        prompt=prompt,
+        negative_prompt=n_prompt,
+        image = image_list,
+        generator = torch.manual_seed(i),
+        num_inference_steps=30,
+    ).images[0]
+    image.save(os.path.join(save_folder, f"seed{i}.png"))
